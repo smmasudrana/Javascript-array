f8e0f84a8fe106b0ef9dc44bc994f3b565b9af30
smmasudrana <smmasudrana62@gmail.com>
2025-07-31 17:17:10 +0600

    add length and index array

---
+++ index.js
@@ -0,0 +1,18 @@
+const number =[35, 63, 64, 346, 36, 36, 36, 63,]
+// !alart index always start with 0 like 0 1 2 3 4 like that.
+
+console.log(number)
+console.log(number[1])  //there is index one is 63 bcz index 0 is 35.
+
+
+// how to a element keep in a variable with array
+const fourth =number[3];
+console.log(fourth);
+
+
+// set or update element value by index
+let abc =45;
+abc=99;
+
+number[0]=999;
+console.log(number[0]);
---
+++ length.js
@@ -0,0 +1,2 @@
+const numbers =[10, 11, 12, 14, 24, 156, 26, 26, 26, 26,264]
+console.log(numbers.length)    //how to know how many item inside the array with length.
