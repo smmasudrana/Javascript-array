d23c6f3abaf658b40ced33d13d7e6b6e2064d371
smmasudrana <smmasudrana62@gmail.com>
2025-07-31 18:04:43 +0600

    add push pop and shift unshift

---
+++ push-pop.js
@@ -0,0 +1,13 @@
+const ages =[]
+const number =[23, 35, 23, 12, 15, 25, 20]
+
+console.log(number);
+number.push(99);      //when you use (.push) it will be add a element in the arrays last.
+console.log(number);
+number.push(38, 100, 64);
+console.log(number);
+
+const fruits =['orange', 'apple', 'pineapple', 'coconut']
+console.log(fruits);
+fruits.pop();         //when you use (.pop) it will be remove the last element from the arrays.
+console.log(fruits);
---
+++ shift-unshift.js
@@ -0,0 +1,13 @@
+const ages =[]
+const number =[23, 35, 23, 12, 15, 25, 20]
+
+console.log(number);
+number.unshift(99);      //when you use (.unshift) it will be add a element in the arrays first.
+console.log(number);
+number.unshift(38, 100, 64);
+console.log(number);
+
+const fruits =['orange', 'apple', 'pineapple', 'coconut']
+console.log(fruits);
+fruits.shift();         //when you use (.pop) it will be remove the first element from the arrays.
+console.log(fruits);
