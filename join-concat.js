f1887e2a7f41220c779e2a9c1bf07e261eed5f89
smmasudrana <smmasudrana62@gmail.com>
2025-07-31 18:34:46 +0600

    add join and concat arrays

---
+++ join-concat.js
@@ -0,0 +1,9 @@
+const friends = ['balam', 'kalam', 'salam', 'gelam']
+
+console.log(friends.join('+'));
+
+
+const name = ['balam', 'kalam', 'salam', 'gelam']
+const upadhi= ['sarker', 'sheikh', 'mirza']
+
+console.log(name.concat(upadhi));   //when you need to add 2 arrays together you should use (concat).
