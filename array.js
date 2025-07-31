984ae48f3f4bdf3263847058e29a7bb862472a13
smmasudrana <smmasudrana62@gmail.com>
2025-07-31 16:56:55 +0600

    add array basics structure

---
+++ array.js
@@ -0,0 +1,12 @@
+// const num1 =10;
+// const num2 =10;
+// const num3 =10;
+// const num4 =10;
+
+const numbers= [10, 11, 12, 13, 14, 15, 16]
+console.log(numbers);
+console.log(length numbers);
+
+const friends =['masud', 'masud', 'masud', 'masud']  //strings
+const passed =[true, false, true, false, false]      //boolean
+const mixed =[12, 'masud', true, 45]
